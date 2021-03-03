5608a9bcce8b0bd8c975a5ba5d59358aa491b891
Angelonobile9 <78024505+Angelonobile9@users.noreply.github.com>
2021-03-03 15:00:26 +0100

    primo commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$("#mainNav").sticky();
+
+$("#about").hide();
+$("#prodotti").hide();
+$("#contatti").hide();
+
+$(".about").on('click', function(){
+    $("#about").show();
+});
+
+$(".prodotti").on('click', function(){
+    $("#prodotti").show();
+});
+
+$(".contatti").on('click', function(){
+    $("#contatti").show();
+});
+
+$(".btn").on('click', function(){
+    $("#contatti").show();
+});
+
