5c747d0184e355b893f139714c5c7b3441be26e7
Angelonobile9 <78024505+Angelonobile9@users.noreply.github.com>
2021-03-09 19:39:42 +0100

    pulizia cartella

--- js/script.js
+++ js/script.js
@@ -1,22 +1,9 @@
 
 $("#mainNav").sticky();
 
-$("#about").hide();
-$("#prodotti").hide();
-$("#contatti").hide();
+  
 
 
-$(".about").on('click', function(){
-    $("#about").show();
-});
-
-$(".prodotti").on('click', function(){
-    $("#prodotti").show();
-});
-
-$(".contatti").on('click', function(){
-    $("#contatti").show();
-});
 
 
 
